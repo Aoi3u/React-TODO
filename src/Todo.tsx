7357ca2de5f58005ede2d670be86cbea7972d222
Aoi3u <miu.blue3@gmail.com>
2025-03-09 17:24:37 +0900

    feature: ひな形の作成

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -2,6 +2,30 @@ import './App.css'
 
 export const Todo = () => {
   return (
-    <div>hoge</div>
+    <>
+      <div>
+        <input placeholder='TODOを入力' />
+        <button>追加</button>
+      </div>
+      <div>
+        <p>未完了のTODO</p>
+        <ul>
+          <li>
+            <p>TODO</p>
+            <button>完了</button>
+            <button>削除</button>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <p>完了のTODO</p>
+        <ul>
+          <li>
+            <p>済</p>
+            <button>戻る</button>
+          </li>
+        </ul>
+      </div>
+    </>
   );
 }
