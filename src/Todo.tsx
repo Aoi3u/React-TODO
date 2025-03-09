7a0d81f8fec4d40c668f6022026c2e17dbc34a50
Aoi3u <miu.blue3@gmail.com>
2025-03-09 18:49:21 +0900

    feature: css実装

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -3,26 +3,30 @@ import './App.css'
 export const Todo = () => {
   return (
     <>
-      <div>
+      <div className='input-area'>
         <input placeholder='TODOを入力' />
         <button>追加</button>
       </div>
-      <div>
-        <p>未完了のTODO</p>
+      <div className='incomplete-area'>
+        <p className='title'>未完了のTODO</p>
         <ul>
           <li>
-            <p>TODO</p>
-            <button>完了</button>
-            <button>削除</button>
+            <div className='list-row'>
+              <p className='todo-item'>TODO</p>
+              <button>完了</button>
+              <button>削除</button>
+            </div>
           </li>
         </ul>
       </div>
-      <div>
-        <p>完了のTODO</p>
+      <div className='complete-area'>
+        <p className='title'>完了のTODO</p>
         <ul>
           <li>
-            <p>済</p>
-            <button>戻る</button>
+            <div className='list-row'>
+              <p className='todo-item'>済</p>
+              <button>戻す</button>
+            </div>
           </li>
         </ul>
       </div>
